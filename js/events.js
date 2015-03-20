bb0bdee22d8e06cecc1ca10582ad4d4ee6efbcb6
Robert Sebescen <pgrobban@gmail.com>
2015-03-20 18:58:24 +0100

    confirm when closing

--- js/events.js
+++ js/events.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
 
+    // closing confirmation
+    var myEvent = window.attachEvent || window.addEventListener;
+    var chkevent = window.attachEvent ? 'onbeforeunload' : 'beforeunload'; /// make IE7, IE8 compitable
+    myEvent(chkevent, function (e) { // For >=IE7, Chrome, Firefox
+        var confirmationMessage = 'Are you sure to leave the page? All unsaved changes will be lost.';  // a space
+        (e || window.event).returnValue = confirmationMessage;
+        return confirmationMessage;
+    });
+
 
     $("#wordClass").change(function () {
         var currentWordClass = $("#wordClass").val();
         app.wordClassChanged(currentWordClass);
     });
-    
-    $("#ignoreArticlesWhenSorting").change(function() { 
+
+    $("#ignoreArticlesWhenSorting").change(function () {
         app.ignoreEntriesWhenSorting = $("#ignoreArticlesWhenSorting").is(':checked');
         // force resort - ugly hack
         app.table.rows().remove().draw(false); // clear the table
@@ -27,7 +36,7 @@ $(document).ready(function () {
     $("#editEntryButton").on("click", app.openEditEntryDialog);
     $("#deleteSelectedEntriesButton").on("click", app.deleteSelectedEntries);
     $("#clearTableButton").on("click", app.clearTable);
-    
+
     $("#exportButton").on('click', app.exportEntriesAsJSON);
     $("#importButton").on("click", function () {
         if (app.entries.length > 0)
@@ -35,7 +44,7 @@ $(document).ready(function () {
                 return;
         $("#fileInput").click();
     });
-    
+
     function errorHandler(error)
     {
         console.log(error);
